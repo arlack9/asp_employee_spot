485e3527b7111d78c45a43f89fdc92189989bfed
arlack9 <alarjun504@gmail.com>
2025-07-04 16:44:23 +0530

    DI the Appdbcontext to EmployeeRepository

--- core2/Repository/EmployeeRepository.cs
+++ core2/Repository/EmployeeRepository.cs
@@ -1,28 +1,34 @@
 ﻿using core2.Models;
+using core2.Repository.Data;
 
-namespace core2.Repository
+namespace core2.Repository;
+
+public class EmployeeRepository : IEmployeeRepository
 {
-    public class EmployeeRepository : IEmployeeRepository
+    private ApplicationDbContext _db;
+    public EmployeeRepository(ApplicationDbContext db)
     {
-        public string AddEmployee(Employee employee)
-        {
-            throw new NotImplementedException();
+        _db = db;
+    }
+    public string AddEmployee(Employee employee)
+    {
+        //throw new NotImplementedException();
+        _db.Employees.Add(employee);
+        
+    }
 
-        }
+    public string AllEmployees()
+    {
+        throw new NotImplementedException();
+    }
 
-        public string AllEmployees()
-        {
-            throw new NotImplementedException();
-        }
+    public string DeleteEmployee(int Id)
+    {
+        throw new NotImplementedException();
+    }
 
-        public string DeleteEmployee(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string UpdateEmployee(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
+    public string UpdateEmployee(Employee employee)
+    {
+        throw new NotImplementedException();
     }
 }
